ac0552d1f5341b1317a1c7d50ed618aa234f9793
michael <michael.gallipo@gmail.com>
2021-05-13 19:02:24 -0400

    Added cors module, reformatted index JSON data

--- routes/state.js
+++ routes/state.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const stateData = require('../formatData.js');
 
 var rawData = stateData.allStateData ?
   stateData.allStateData : "no Data Available";
 
-  router.get('/', (req, res) => {
+  router.get('/', cors(), (req, res) => {
     // res.send('It Works!');
-    res.send(rawData._rejectionHandler0);
+    var mapData = {};
+    rawData._rejectionHandler0.forEach(state => {
+      mapData[state.stateCode] = {
+        "state": state.state,
+        "median_household_income": state.median_household_income,
+        "share_unemployed_seasonal": state.share_unemployed_seasonal,
+        "share_population_in_metro_areas": state.share_population_in_metro_areas,
+        "share_population_with_high_school_degree": state.share_population_with_high_school_degree,
+      }
+    })
+    res.send(mapData);
   });
 
   router.get('/:stateId', (req, res) => {
